f6d88ec4688214d85ea753860ebb2eb396a8fbe0
Kostya05A <61642989+Kostya05A@users.noreply.github.com>
2023-12-01 16:20:28 +0700

    Create your_app_file.js

---
+++ your_app_file.js
@@ -0,0 +1,7 @@
+const server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'application/json');
+res.end(JSON.stringify({message: "Hello, docker"}));
+});
+
+server.listen(5050, () => console.log('Http server started!'));
